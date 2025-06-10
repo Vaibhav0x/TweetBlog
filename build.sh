f7ecc5daaec97188cd9c7b0f1ff2cb1926bcb59d
Vaibhav0x <rajjvaibhavv121@gmail.com>
2025-06-11 00:01:42 +0530

    build sh file added migrations command

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ pip install -r requirements.txt
 # collect static files
 python manage.py collectstatic --noinput
 
+# apply migrations before migrate the new fields
+python manage.py makemigrations
+
 # apply database migrations
 python manage.py migrate
